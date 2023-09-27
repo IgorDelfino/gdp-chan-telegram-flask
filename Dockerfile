508d0af33283d8c6be9a932ffec0714cf68a83f4
Igor_Delfino <igor.delfino@pluga.co>
2023-09-27 20:54:25 -0300

    first stable build

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,6 @@ COPY . .
 ENV FLASK_APP ./gdp-chan/index.py
 ENV BOT_TOKEN 626130318:AAF0CCD1hnKmnxhQCcZTHRSSC8jDlSeOXkQ
 ENV PORT 8080
+ENV SERVER_URL https://gdp-chan-p67toivrga-rj.a.run.app
 
 CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 gdp-chan.index:app
--- gdp-chan/index.py
+++ gdp-chan/index.py
@@ -1,8 +1,15 @@
 import requests
 import os
-from flask import Flask
+import telebot
+from flask import Flask, request
 
-anime_api_base_url = 'https://api.jikan.moe/v4'
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+bot.set_webhook(url=os.environ.get('SERVER_URL') + "/webhook_receiver")
+
+webhook_log = []
 
 def request_handler(base_uri, request_route):
   request = requests.get(base_uri + request_route).json();
@@ -10,14 +17,39 @@ def request_handler(base_uri, request_route):
 
 app = Flask(__name__)
 
-@app.route("/")
-def ping():
-  target = os.environ.get('TARGET', 'World')
-  return 'Hello {}!\n'.format(target)
+def send_message(chat_id, text):
+  bot.send_message(chat_id=chat_id, text=text);
 
-@app.route("/get_anime")
-def get_anime():
-  return request_handler(anime_api_base_url, '/random/anime')
+@app.route("/webhook_receiver",methods = ['POST'])
+def webhook_treater():
+  json_request = request.get_json()
+  if "message" in json_request and "text" in json_request["message"]:
+    try:
+      message = json_request["message"]
+      chat = message["chat"]
+      chat_id = chat["id"]
+      command = message["text"]
+
+      if command == '/gdp':
+        webhook_log.append(json_request)
+        send_message(chat_id=chat_id, text='Eu sou a GDP-chan! :D')  
+
+        return 'Message sent!'
+      else:
+        print('comando: ' + command)
+        return 'Command not valid!'
+      
+    except:
+      print(json_request)
+      return 'Could not treat message!'
+  return 'Unsuported message'
+
+
+@app.route("/webhook_logger")
+def webhook_logger():
+  return webhook_log
+
+   
 
 if __name__ == "__main__":
     app.run(debug=True,host='localhost',port=int(os.environ.get('PORT', 8080)))
